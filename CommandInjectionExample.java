13630bf771a3250b7214edd48e1bcd6b7de64015
fgomez1985 <fgomez@fluidattacks.com>
2025-08-07 18:02:40 -0600

    Update CommandInjectionExample.java

--- CommandInjectionExample.java
+++ CommandInjectionExample.java
@@ -1,4 +1,4 @@
-// Ejemplo vulnerable: Command Injection
+#// Ejemplo vulnerable: Command Injection
 public class CommandInjectionExample {
     public static void main(String[] args) throws Exception {
         String userInput = args.length > 0 ? args[0] : "ls";
